d0d6cca9dab4915f547e95de59944addb643466b
hartron18 <okaforharry18@gmail.com>
2022-05-25 09:39:27 +0100

    Initial Commit

---
+++ SkinetApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkinetApi.Entities;
+
+namespace SkinetApi.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreDbContext context;
+
+        public ProductController(StoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            var product = await context.Products.ToListAsync();
+
+            return Ok(product);
+        }
+
+
+    }
+}
---
+++ SkinetApi/StoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkinetApi.Entities;
+
+namespace SkinetApi
+{
+    public class StoreDbContext: DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var product1 = new Product { Id = 1, Name = "Product one" };
+            var product2 = new Product { Id = 2, Name = "Product two" };
+            var product3 = new Product { Id = 3, Name = "Product three" };
+
+            modelBuilder.Entity<Product>().HasData(product1, product2, product3);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
